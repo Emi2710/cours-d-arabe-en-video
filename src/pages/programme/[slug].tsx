77d07a9a5dfe93a1b11178d8379b426004785a6f
Emi2710 <deadourfreedom@gmail.com>
2023-10-04 14:17:19 +0200

    fixed bug on chapter display
    the chapter name only shows once when consecutive lessons are displayed instead of each time

--- src/pages/programme/[slug].tsx
+++ src/pages/programme/[slug].tsx
@@ -209,6 +209,7 @@ const renderOrderByPublicationView = () => {
     .sort((a, b) => new Date(a.publishedAt).getTime() - new Date(b.publishedAt).getTime());
 
 
+    let previousChapterName = ''; // Variable pour stocker le nom du chapitre précédent
 
 
 
@@ -407,38 +408,45 @@ const renderOrderByPublicationView = () => {
         </button>
 
         {/* Render lessons based on the selected view */}
-        {showOrderByPublication
-          ? sortedLessons.map((lesson) => {
-              // Function to find the parent course of the lesson
-              const findParentCourse = () => {
-                for (const course of programme.cours) {
-                  if (
-                    course.lesson &&
-                    course.lesson.some((coursLesson) => coursLesson.name === lesson.name)
-                  ) {
-                    return course.name; // Return the course name as the chapter name
+          {showOrderByPublication
+            ? sortedLessons.map((lesson) => {
+                // Function to find the parent course of the lesson
+                const findParentCourse = () => {
+                  for (const course of programme.cours) {
+                    if (
+                      course.lesson &&
+                      course.lesson.some((coursLesson) => coursLesson.name === lesson.name)
+                    ) {
+                      return course.name; // Return the course name as the chapter name
+                    }
                   }
-                }
-                return ''; // Default to an empty string if no parent course is found
-              };
+                  return ''; // Default to an empty string if no parent course is found
+                };
 
-              const chapterName = findParentCourse(); // Get the chapter name dynamically
+                const chapterName = findParentCourse(); // Get the chapter name dynamically
 
-              return (
-                <div key={lesson.name}>
-                  <h3>{chapterName}</h3>
-                  <Lesson
-                    key={lesson.name}
-                    name={lesson.name}
-                    lessonLink={lesson.lessonLink}
-                    pdfLink={lesson.pdfLink}
-                    type={lesson.type}
-                    publishedAt={lesson.publishedAt}
-                  />
-                </div>
-              );
-            })
-          : renderDefaultView()}
+                // Check if the chapter name is different from the previous one
+                const isDifferentChapter = chapterName !== previousChapterName;
+
+                // Update the previousChapterName with the current chapter name
+                previousChapterName = chapterName;
+
+                return (
+                  <div key={lesson.name}>
+                    {/* Afficher le nom du chapitre uniquement s'il est différent du précédent */}
+                    {isDifferentChapter && <h3>{chapterName}</h3>}
+                    <Lesson
+                      key={lesson.name}
+                      name={lesson.name}
+                      lessonLink={lesson.lessonLink}
+                      pdfLink={lesson.pdfLink}
+                      type={lesson.type}
+                      publishedAt={lesson.publishedAt}
+                    />
+                  </div>
+                );
+              })
+            : renderDefaultView()}
 
       </div>  
         </div>
