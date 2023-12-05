e47c581424442cdd3eef0e175af89f9e8af29ed3
Emi2710 <deadourfreedom@gmail.com>
2023-12-05 23:08:52 +0100

    implement Google Analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,30 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="fr">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="fr">
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-4MCD5H3SSR"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-4MCD5H3SSR');
+              `,
+            }}
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
